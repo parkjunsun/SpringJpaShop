5584626936c590f2e237032d9b082ee28c0bdbb3
park <qkrwnstns52@naver.com>
2021-02-15 16:38:43 +0900

    commit

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,65 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     *
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+    /**
+     * 주문 검색
+     */
+
+
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
+
+}
